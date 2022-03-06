fa19e8f4533ccaf11f43832d4cf92a7b55b8bef9
Javid <Medz_rp99@itstep.edu.az>
2022-03-06 14:10:33 +0400

    Modefied UI

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WaterResourcesManager.Models.Abstract;
+using WaterResourcesManager.Models.Concrete;
 
 namespace UI
 {
@@ -31,6 +33,77 @@ namespace UI
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
             );
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ShowWaterDistrib();
+        }
+
+        private void ShowWaterDistrib(Dictionary<WaterResource, double> waterDistrib)
+        {
+            for (int i = 0; i < waterDistrib.Count; i++)
+            {
+                string waterResourceName = waterDistrib.ElementAt(i).Key.Name;
+                string waterAmountValue = waterDistrib.ElementAt(i).Value.ToString();
+                string phLevelValue = waterDistrib.ElementAt(i).Key.PollutionLevel.ToString();
+
+                RowDefinition row = new RowDefinition();
+                row.Height = GridLength.Auto;
+                waterResultsGrid.RowDefinitions.Add(row);
+                Grid gridAns = new Grid();
+                gridAns.Name = $"GridAns{i + 1}";
+                gridAns.SetValue(Grid.RowProperty, i);
+                ColumnDefinition gridCol1 = new ColumnDefinition();
+                gridCol1.Width = new GridLength(1, GridUnitType.Star);
+                ColumnDefinition gridCol2 = new ColumnDefinition();
+                gridCol2.Width = new GridLength(1, GridUnitType.Star);
+                ColumnDefinition gridCol3 = new ColumnDefinition();
+                gridCol3.Width = new GridLength(1, GridUnitType.Star);
+                gridAns.ColumnDefinitions.Add(gridCol1);
+                gridAns.ColumnDefinitions.Add(gridCol2);
+                gridAns.ColumnDefinitions.Add(gridCol3);
+                gridAns.Visibility = Visibility.Collapsed;
+                RegisterName($"GridAns{i + 1}", gridAns);
+
+                TextBlock waterResource = new TextBlock();
+                waterResource.SetValue(Grid.ColumnProperty, 1);
+                waterResource.Name = $"WaterResource{i + 1}";
+                waterResource.TextWrapping = TextWrapping.Wrap;
+                waterResource.HorizontalAlignment = HorizontalAlignment.Stretch;
+                waterResource.VerticalAlignment = VerticalAlignment.Center;
+                waterResource.Margin = new Thickness(10);
+                waterResource.FontSize = 17;
+                waterResource.Text = waterResourceName;
+                RegisterName($"WaterResource{i + 1}", waterResource);
+
+                TextBlock waterAmount = new TextBlock();
+                waterAmount.SetValue(Grid.ColumnProperty, 1);
+                waterAmount.Name = $"WaterAmount{i + 1}";
+                waterAmount.TextWrapping = TextWrapping.Wrap;
+                waterAmount.HorizontalAlignment = HorizontalAlignment.Stretch;
+                waterAmount.VerticalAlignment = VerticalAlignment.Center;
+                waterAmount.Margin = new Thickness(10);
+                waterAmount.FontSize = 17;
+                waterAmount.Text = waterAmountValue;
+                RegisterName($"WaterAmount{i + 1}", waterAmount);
+
+                TextBlock phLevel = new TextBlock();
+                phLevel.SetValue(Grid.ColumnProperty, 1);
+                phLevel.Name = $"phLevel{i + 1}";
+                phLevel.TextWrapping = TextWrapping.Wrap;
+                phLevel.HorizontalAlignment = HorizontalAlignment.Stretch;
+                phLevel.VerticalAlignment = VerticalAlignment.Center;
+                phLevel.Margin = new Thickness(10);
+                phLevel.FontSize = 17;
+                phLevel.Text = phLevelValue;
+                RegisterName($"phLevel{i + 1}", phLevel);
+
+                gridAns.Children.Add(waterResource);
+                gridAns.Children.Add(waterAmount);
+                gridAns.Children.Add(phLevel);
+                waterResultsGrid.Children.Add(gridAns);
+            }
         }
     }
 }
